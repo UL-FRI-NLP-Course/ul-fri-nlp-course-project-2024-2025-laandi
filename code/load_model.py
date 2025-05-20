ff808cb4fe83b3f059b98750432422fa8c3d40a5
Lara Anžur <la4710@hpc-login3.arnes.si>
2025-05-20 15:08:26 +0200

    loading models locally

---
+++ code/load_model.py
@@ -0,0 +1,10 @@
+from huggingface_hub import snapshot_download
+
+local_path = snapshot_download(
+    repo_id="sentence-transformers/all-mpnet-base-v2",
+    cache_dir="./models/hf-models",  # or another persistent folder
+    local_dir="./models/sentence-transformer",  # where to store the final files
+    local_dir_use_symlinks=False  # copy files instead of symlinks
+)
+
+print(f"Model downloaded to: {local_path}")
