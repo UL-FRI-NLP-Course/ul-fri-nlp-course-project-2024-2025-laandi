dd6a0e62574195bdae1838a4fd05de4c949a07d6
laraanzur <lara.anzur@gmail.com>
2025-05-25 13:38:33 +0200

    instructions for code

--- code/baseline.py
+++ code/baseline.py
@@ -4,9 +4,9 @@ def ask_dynamic_rag(question, keywords=None, searchNewPaper=False):
     model = dynamic_rag_model.DynamicRAG()
     return model.query(question, keywords, searchNewPaper)
 
-
-ask_dynamic_rag(
-    question="How are large language models used in making different transcripts?",
-    keywords=None,
-    searchNewPaper=True
-)
+# # Testing
+# ask_dynamic_rag(
+#     question="How are large language models used in making different transcripts?",
+#     keywords=None,
+#     searchNewPaper=True
+# )
